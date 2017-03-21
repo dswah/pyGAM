cd6d8cbf50ff52bda45705cbecb13757a3796943
Daniel Serven <dserven@gmail.com>
2017-03-21 10:48:26 -0700

    added poission and gamma dists, log and inverse links

--- pygam/links.py
+++ pygam/links.py
@@ -24,13 +24,14 @@ class IdentityLink(Link):
 
     def mu(self, lp, dist):
         """
-        glm mean ie inverse of link function
+        glm mean function, ie inverse of link function
+        this is useful for going from the linear prediction to mu
         """
         return lp
 
     def gradient(self, mu, dist):
         """
-        derivative of the linear prediction wrt mu
+        derivative of the link function wrt mu
         """
         return np.ones_like(mu)
 
@@ -47,14 +48,62 @@ class LogitLink(Link):
 
     def mu(self, lp, dist):
         """
-        glm mean ie inverse of link function
-        for classification this is the prediction probabilities
+        glm mean function, ie inverse of link function
+        this is useful for going from the linear prediction to mu
         """
         elp = np.exp(lp)
         return dist.levels * elp / (elp + 1)
 
     def gradient(self, mu, dist):
         """
-        derivative of the linear prediction wrt mu
+        derivative of the link function wrt mu
         """
         return dist.levels/(mu*(dist.levels - mu))
+
+class LogLink(Link):
+    def __init__(self):
+        super(LogLink, self).__init__(name='log')
+
+    def link(self, mu, dist):
+        """
+        glm link function
+        this is useful for going from mu to the linear prediction
+        """
+        return np.log(mu)
+
+    def mu(self, lp, dist):
+        """
+        glm mean function, ie inverse of link function
+        this is useful for going from the linear prediction to mu
+        """
+        return np.exp(lp)
+
+    def gradient(self, mu, dist):
+        """
+        derivative of the link function wrt mu
+        """
+        return 1. / mu
+
+class Inverse(Link):
+    def __init__(self):
+        super(Inverse, self).__init__(name='inverse')
+
+    def link(self, mu, dist):
+        """
+        glm link function
+        this is useful for going from mu to the linear prediction
+        """
+        return mu**-1
+
+    def mu(self, lp, dist):
+        """
+        glm mean function, ie inverse of link function
+        this is useful for going from the linear prediction to mu
+        """
+        return lp**-1
+
+    def gradient(self, mu, dist):
+        """
+        derivative of the link function wrt mu
+        """
+        return -1 * mu**-2
