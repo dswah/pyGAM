35e5d6e0b2644e0c82c542dfe201862096115349
Daniel Serven <dserven@gmail.com>
2017-04-12 23:48:21 -0700

    travis, reqs, tests, datasets, pylint

---
+++ pygam/tests/conftest.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import pandas as pd
+import numpy as np
+
+from pygam import *
+
+
+@pytest.fixture
+def mcycle():
+    # y is real
+    # recommend LinearGAM
+    motor = pd.read_csv('../../datasets/mcycle.csv', index_col=0)
+    X = motor.times.values
+    y = motor.accel
+    return X, y
+
+@pytest.fixture
+def coal():
+    # y is counts
+    # recommend PoissonGAM
+    coal = pd.read_csv('../../datasets/coal.csv', index_col=0)
+    y, x = np.histogram(coal.values, bins=150)
+    X = x[:-1] + np.diff(x)/2 # get midpoints of bins
+    return X, y
+
+@pytest.fixture
+def faithful():
+    # y is counts
+    # recommend PoissonGAM
+    faithful = pd.read_csv('../../datasets/faithful.csv', index_col=0)
+    y, x = np.histogram(faithful.values, bins=200)
+    X = x[:-1] + np.diff(x)/2 # get midpoints of bins
+    return X, y
+
+@pytest.fixture
+def wage():
+    # y is real
+    # recommend LinearGAM
+    wage = pd.read_csv('../../datasets/wage.csv', index_col=0)
+    X = wage[['year', 'age', 'education']].values
+    X[:,-1] = np.unique(X[:,-1], return_inverse=True)[1]
+    y = wage['wage'].values
+    return X, y
+
+@pytest.fixture
+def trees():
+    # y is real.
+    # recommend InvGaussGAM, or GAM(distribution='gamma', link='log')
+    trees = pd.read_csv('../../datasets/trees.csv', index_col=0)
+    y = trees.Volume.values
+    X = trees[['Girth', 'Height']].values
+    return X, y
+
+@pytest.fixture
+def default():
+    # y is binary
+    # recommend LogisticGAM
+    default = pd.read_csv('../../datasets/Default.csv', index_col=0)
+    default = default.values
+    default[:,0] = np.unique(default[:,0], return_inverse=True)[1]
+    default[:,1] = np.unique(default[:,1], return_inverse=True)[1]
+    X = default[:,1:]
+    y = default[:,0]
+    return X, y
+
+@pytest.fixture
+def cake():
+    # y is real
+    # recommend LinearGAM
+    cake = pd.read_csv('../../datasets/cake.csv', index_col=0)
+    X = cake[['recipe', 'replicate', 'temperature']].values
+    X[:,0] = np.unique(cake.values[:,1], return_inverse=True)[1]
+    X[:,1] -= 1
+    y = cake['angle'].values
+    return X, y
---
+++ pygam/tests/test_GAMs.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from pygam import *
+
+
+def test_can_build_sub_models():
+    """
+    check that the inits of all the sub-models are correct
+    """
+    LinearGAM()
+    LogisticGAM()
+    PoissonGAM()
+    GammaGAM()
+    InvGaussGAM()
+    assert(True)
+
+def test_LinearGAM_uni(mcycle):
+    """
+    check that we can fit a Linear GAM on real, univariate data
+    """
+    X, y = mcycle
+    gam = LinearGAM().fit(X, y)
+    assert(gam._is_fitted)
+
+def test_LinearGAM_multi(wage):
+    """
+    check that we can fit a Linear GAM on real, multivariate data
+    """
+    X, y = wage
+    gam = LinearGAM().fit(X, y)
+    assert(gam._is_fitted)
+
+def test_LogisticGAM(default):
+    """
+    check that we can fit a Logistic GAM on real data
+    """
+    X, y = default
+    gam = LogisticGAM().fit(X, y)
+    assert(gam._is_fitted)
+
+def test_PoissonGAM(coal):
+    """
+    check that we can fit a Poisson GAM on real data
+    """
+    X, y = coal
+    gam = PoissonGAM().fit(X, y)
+    assert(gam._is_fitted)
+
+def test_InvGaussGAM(trees):
+    """
+    check that we can fit a InvGauss GAM on real data
+    """
+    X, y = trees
+    gam = InvGaussGAM().fit(X, y)
+    assert(gam._is_fitted)
+
+def test_GammaGAM(trees):
+    """
+    check that we can fit a Gamma GAM on real data
+    """
+    X, y = trees
+    gam = GammaGAM().fit(X, y)
+    assert(gam._is_fitted)
+
+def test_CustomGAM(trees):
+    """
+    check that we can fit a Custom GAM on real data
+    """
+    X, y = trees
+    gam = GAM(distribution='gamma', link='log').fit(X, y)
+    assert(gam._is_fitted)
