33b426c9fdbaf0f3476f80c649ccf4c4adbd710b
Daniel Serven <dserven@gmail.com>
2017-08-02 23:39:32 -0700

    remove exceptions module

--- pygam/exceptions.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-class NotPositiveDefiniteError(ValueError):
-    """Exception class to raise if a matrix is not positive definite
-    """
-
-class NotFiniteError(ValueError):
-    """Exception class to raise if a matrix contains NaN of Inf values
-    """
