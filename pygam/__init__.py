6ef34802101f629adb54225f3527ad1e1ed6f0ea
daniel servén <dserven@gmail.com>
2018-04-04 15:21:09 -0700

    bump patch number

--- pygam/__init__.py
+++ pygam/__init__.py
@@ -14,4 +14,4 @@ from pygam.pygam import InvGaussGAM
 __all__ = ['GAM', 'LinearGAM', 'LogisticGAM', 'GammaGAM', 'PoissonGAM',
            'InvGaussGAM']
 
-__version__ = '0.4.1'
+__version__ = '0.4.2'
