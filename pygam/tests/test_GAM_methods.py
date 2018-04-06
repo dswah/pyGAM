4c159a6f3c7a63e5f35fbb7fefa4497ab3616104
dani <dserven@gmail.com>
2018-04-05 22:22:10 -0700

    update test to account for more lines of summary output

--- pygam/tests/test_GAM_methods.py
+++ pygam/tests/test_GAM_methods.py
@@ -197,7 +197,7 @@ def test_summary_returns_12_lines(mcycle_gam):
     stdout = sys.stdout  #keep a handle on the real standard output
     sys.stdout = StringIO() #Choose a file-like object to write to
     mcycle_gam.summary()
-    assert(len(sys.stdout.getvalue().split('\n')) == 16)
+    assert(len(sys.stdout.getvalue().split('\n')) == 24)
 
 def test_is_fitted_predict(mcycle):
     """
