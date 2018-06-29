cac52317eb84c6a36bd123cb6203be0218b0fc55
dani <dserven@gmail.com>
2018-06-29 16:22:06 +0200

    add regression test

--- pygam/tests/test_GAM_methods.py
+++ pygam/tests/test_GAM_methods.py
@@ -513,3 +513,20 @@ def test_2d_y_still_allow_fitting_in_PoissonGAM(coal_X_y):
     # 2d exposure should cause no problems now
     gam = PoissonGAM().fit(X, y, exposure=two_d_data)
     assert gam._is_fitted
+
+def test_non_int_exposure_produced_no_inf_in_PoissonGAM_ll(coal_X_y):
+    """
+    regression test.
+
+    there was a bug where we forgot to round the rescaled counts before
+    computing the loglikelihood. since Poisson requires integer observations,
+    small numerical errors caused the pmf to return -inf, which shows up
+    in the loglikelihood computations, AIC, AICc..
+    """
+    X, y = coal_X_y
+
+    rate = 1.2 + np.cos(np.linspace(0, 2. * np.pi, len(y)))
+
+    gam = PoissonGAM().fit(X, y, exposure=rate)
+
+    assert np.isfinite(gam.statistics_['loglikelihood'])
