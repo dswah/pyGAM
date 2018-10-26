1f93d9841ac03a4573486168bbf55378d4838cb3
daniel servén <dserven@gmail.com>
2018-10-26 13:37:09 +0200

    bump patch number

--- pygam/__init__.py
+++ pygam/__init__.py
@@ -21,4 +21,4 @@ from pygam.terms import intercept
 __all__ = ['GAM', 'LinearGAM', 'LogisticGAM', 'GammaGAM', 'PoissonGAM',
            'InvGaussGAM', 'ExpectileGAM', 'l', 's', 'f', 'te', 'intercept']
 
-__version__ = '0.7.1'
+__version__ = '0.7.2'
