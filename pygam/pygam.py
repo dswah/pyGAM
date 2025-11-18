f031c13e7f5b34f7b3f61f097c66d487e573b4b9
daniel servén <dserven@gmail.com>
2025-11-18 12:53:52 +0100

    fix linting

--- pygam/pygam.py
+++ pygam/pygam.py
@@ -1929,7 +1929,7 @@ class GAM(Core, MetaTermMixin):
         y = check_y(y, self.link, self.distribution, verbose=self.verbose)
         X = check_X(X, verbose=self.verbose)
         check_X_y(X, y)
-        
+
         # special checks if model not fitted
         if not self._is_fitted:
             self._validate_data_dep_params(X)
