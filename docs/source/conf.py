c7ed700e5df984c1fdc503bd346a161afc0e0f9e
dani serven <dserven@gmail.com>
2025-11-28 13:14:03 +0100

    try with autoapi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@
 # from pathlib import Path
 #
 # sys.path.insert(0, str(Path("../..").resolve()))
-autoapi_generate_api_docs = False
+# autoapi_generate_api_docs = False
 autoapi_dirs = ["../../pygam"]
 
 project = "pyGAM"
