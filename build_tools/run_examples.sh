1be4a957e6f6397fad19ddec8d2d13e645a38893
Franz Király <fkiraly@gcos.ai>
2025-12-24 00:27:17 +0100

    Update run_examples.sh

--- build_tools/run_examples.sh
+++ build_tools/run_examples.sh
@@ -3,7 +3,12 @@
 # Script to run all example notebooks.
 set -euxo pipefail
 
-CMD="jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=600"
+CMD="jupyter nbconvert \
+  --to notebook \
+  --inplace \
+  --execute \
+  --ExecutePreprocessor.timeout=600 \
+  --ExecutePreprocessor.kernel_name=python3"
 
 for notebook in docs/notebooks/*.ipynb; do
   echo "Running: $notebook"
