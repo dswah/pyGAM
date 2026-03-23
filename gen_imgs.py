fd7ebeec398da5d667d22cad780582b276debc4f
Kinara2020 <pkinara2020@gmail.com>
2026-03-23 17:16:12 +0530

    Fix matplotlib backend for CI to avoid Tkinter display errors

--- gen_imgs.py
+++ gen_imgs.py
@@ -1,5 +1,6 @@
 """Generate some plots for the pyGAM repo."""
-
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.font_manager import FontProperties
